e0100afbd1025171bc286ae3ddec95c98f93ad25
Darvey <david.perrey@gmail.com>
2016-01-19 20:45:23 +0100

    Ajout début formulaire
    Ajout du début du formulaire de connexion dans concert.php

--- connect.php
+++ connect.php
@@ -2,5 +2,30 @@
   require_once 'includes/htmlElmt.php';
 
   displayHead("connect");
+  echo '<body>';
   displayHeader();
 ?>
+
+
+
+<div id = "form-connect">
+  <div class ="bloc-connect">
+    <form method="post" action="traitement.php">
+           <label for="email">Email </label>
+           <input type="text" name="s_mail" id="s_mail" />
+           <br />
+           <label for="pass">Mot de passe </label>
+           <input type="password" name="s_pass" id="s_pass" />
+    </form>
+  </div>
+
+  <div class = "bloc-inscription">
+  </div>
+</div>
+
+<?php
+  displayFooter();
+?>
+
+</body>
+</html>
