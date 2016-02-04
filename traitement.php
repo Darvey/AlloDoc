aeb14a7c28b2668545cbf66c0ebd6097390b4a6d
azerpy <pyjonathan31@gmail.com>
2016-02-04 17:36:37 +0100

    pb sommaire

--- traitement.php
+++ traitement.php
@@ -2,20 +2,24 @@
 
 	// on se connecte à MySQL 
 	$db = mysql_connect('localhost', 'root', 'root'); 
-	mysql_select_db('docapp',$db); 
-
+	mysql_select_db('docapp',$db);
+		
 	if(!empty($_POST['s_mail']) && !empty($_POST['s_pass'])) {
-		//$password = $POST['s_pass'];
-		//print_r($_POST);
+	
 		$password = md5($_POST['s_pass']);
 		$mail = $_POST['s_mail'];
 		
-		$sql = "select p_mdp from patient where p_mail='".$mail."'";
-		$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+		/*$sql = "select p_mdp from patient where p_mail='".$mail."'";
+		$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());*/
+		
+		$data = $db->prepare( 'select p_mdp from patient where p_mail = (:mail)' ); 
+        $data->bindParam(':mail', $mail); 
+        $data->execute(); 
+        $row = $data->fetch(); 
 
-		$data = mysql_fetch_assoc($req);
+		//$data = mysql_fetch_assoc($req);
 
-		if($data['p_mdp'] != $password) {
+		if($row['p_mdp'] != $password) {
 			echo 'Probleme Mail/Mot de passe';
 		} else{
 			echo "Vous etes connecté";
