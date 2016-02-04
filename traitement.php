e657aa08ae0b7ea4fe5f15d67996c286b1f6f35e
azerpy <pyjonathan31@gmail.com>
2016-02-04 18:58:17 +0100

    protection connexion
    sécurisation de la connexion

--- traitement.php
+++ traitement.php
@@ -1,30 +1,40 @@
 <?php if(!empty($_POST['Envoyer'])){ // Si le formulaire est envoyé.
 
-	// on se connecte à MySQL 
-	$db = mysql_connect('localhost', 'root', 'root'); 
-	mysql_select_db('docapp',$db);
+	// on se connecte à MySQL 	
+	$mysqli = new mysqli("localhost", "root", "root", "docapp");
+	
+	if ($mysqli->connect_errno) {
+		echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
 		
 	if(!empty($_POST['s_mail']) && !empty($_POST['s_pass'])) {
 	
 		$password = md5($_POST['s_pass']);
 		$mail = $_POST['s_mail'];
-		
-		/*$sql = "select p_mdp from patient where p_mail='".$mail."'";
-		$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());*/
-		
-		$data = $db->prepare( 'select p_mdp from patient where p_mail = (:mail)' ); 
-        $data->bindParam(':mail', $mail); 
-        $data->execute(); 
-        $row = $data->fetch(); 
 
-		//$data = mysql_fetch_assoc($req);
+		if (!($data = $mysqli->prepare( "SELECT p_mdp FROM patient WHERE p_mail = (?) LIMIT 1;"))) {
+			echo "Echec de la préparation : (" . $mysqli->errno . ") " . $mysqli->error;
+		};
+        if (!($data->bind_param("i", $mail))){
+			echo "Echec lors du liage des paramètres : (" . $data->errno . ") " . $data->error;
+		};
+        if(!($data->execute())){
+			echo "Echec lors de l'exécution : (" . $data->errno . ") " . $data->error;
+		};
+		
+		$data->bind_result($name);
+		
+		// Lecture des valeurs 
+		while ($data->fetch()) {
+			$mdp =  $name;
+		}
 
-		if($row['p_mdp'] != $password) {
+		if($mdp != $password) {
 			echo 'Probleme Mail/Mot de passe';
 		} else{
 			echo "Vous etes connecté";
 		}
-		
 	}
 }
 ?>
