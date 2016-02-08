e0f19e379412b97ff2f3e508209cfd03ee3f252c
Darvey <david.perrey@gmail.com>
2016-02-08 17:25:46 +0100

    légères modifications

--- compteMedecin.php
+++ compteMedecin.php
@@ -11,22 +11,40 @@
 </head>
 
 <body>
+
+<?php
+  require_once 'includes/config.php';
+  $month = false;
+  $year = false;
+  $day = null;
+
+  if ( isset($_GET['m']) ){
+    $month = $_GET['m'];
+  }
+  if ( isset($_GET['y']) )  {
+    $year = $_GET['y'];
+  }
+  if ( isset ($_GET['d']) ){
+    $day = $_GET['d'];
+  }
+?>
+
 	<header>
 		<a class="title" href="index.php"> Allodoc. </a>
 	</header>
 
 	<div class="header-menu">
-		<a> Patients <img src="img/menu-cursor.png" class="menu-select1"/> </a> 
-		<a> Agenda <img src="img/menu-cursor.png" class="menu-select2"/> </a> 
-		<a> Mon compte <img src="img/menu-cursor.png" class="menu-select3"/> </a> 
+		<a> Patients <img src="img/menu-cursor.png" class="menu-select1"/> </a>
+		<a> Agenda <img src="img/menu-cursor.png" class="menu-select2"/> </a>
+		<a> Mon compte <img src="img/menu-cursor.png" class="menu-select3"/> </a>
 	</div>
 
 	<section>
 	</section>
 
 	<div class="agenda-container">
-		<img src="img/pastButton.svg" class="lastButton">
-		<img src='img/nextButton.svg' class="nextButton">
+		<a href=""><img src="img/pastButton.svg" class="lastButton"></a>
+		<a href=""><img src='img/nextButton.svg' class="nextButton"></a>
 		<div class="agenda-header">
 			<div class ="agenda-header-day">
 				Lundi <span>12</span>
@@ -48,6 +66,64 @@
 			</div>
 		</div>
 		<div class="agenda-body">
+         <table>
+            <tr>
+
+               <td>
+                  <span class='rdv-time'> 8h30 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+                  <div class='rdv-hover'>
+                  </div>
+                  <span class='rdv-time'> 9h30 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+                  <div class='rdv-hover'>
+                  </div>
+                  <span class='rdv-time'> 10h00 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+
+               </td>
+
+               <td>
+                  <span class='rdv-time'> 8h30 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+                  <div class='rdv-hover'>
+                  </div>
+               </td>
+
+               <td>
+               </td>
+
+               <td>
+               </td>
+
+               <td>
+                  <span class='rdv-time'> 8h30 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+                  <div class='rdv-hover'>
+                  </div>
+                  <span class='rdv-time'> 11h00 </span>
+                  <br>
+                  <span class='rdv-name'> Mr Hassan Mountassir </span>
+                  <hr>
+                  <div class='rdv-hover'>
+                  </div>
+               </td>
+
+               <td>
+               </td>
+
+            </tr>
+         </table>
 		</div>
 	</div>
 
@@ -55,5 +131,3 @@
 	</footer>
 
 </body>
-
-
